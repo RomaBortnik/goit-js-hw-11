5c7843a88e020928c116cb11eec44a76b018b1b6
RomaBortnik <bortnik25051998@gmail.com>
2023-03-12 16:00:22 +0200

    made main part + simpleloghtbox

--- src/fetch-photos.js
+++ src/fetch-photos.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { countOfPage } from '.';
+import { countPerPage } from '.';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const PERSONAL_KEY = '34315527-c905e7d0ccc489dbd5469a006';
@@ -10,8 +12,8 @@ export async function fetchPhotos(value) {
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
-    page: '1',
-    per_page: '40',
+    page: `${countOfPage.toString()}`,
+    per_page: `${countPerPage}`,
   });
 
   const response = await axios.get(`${BASE_URL}?${params}`);
--- src/index.js
+++ src/index.js
@@ -1,30 +1,72 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// import InfiniteScroll from 'infinite-scroll';
 import { fetchPhotos } from './fetch-photos';
+export let countOfPage = 1;
+export let countPerPage = 40;
 
 const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
-const inputEl = document.querySelector('[name="searchQuery"]');
-const loadButton = document.querySelector('.load-button');
+const loadButton = document.querySelector('.gallery__load-button');
+
+let inputDefaultValue = null;
+let totalHits = null;
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 searchForm.addEventListener('submit', onFormSubmit);
+loadButton.addEventListener('click', onLoadButtonClick);
 
 function onFormSubmit(event) {
   event.preventDefault();
-  const inputValue = event.currentTarget.elements.searchQuery.value;
+
+  inputValue = event.currentTarget.elements.searchQuery.value;
+  if (inputDefaultValue === inputValue) {
+    return;
+  }
+  inputDefaultValue = inputValue;
+  makeInitialValues();
 
   fetchPhotos(inputValue)
     .then(data => {
       console.log(data);
-      if (data.hits.length === 0) {
+      if (data.totalHits === 0) {
         return Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
         );
       }
 
+      Notify.info(`Hooray! We found ${data.totalHits} images.`);
       createMarkup(data);
+      lightbox.refresh();
 
-      loadButton.classList.remove('visually-hidden');
-      inputEl.value = '';
+      if (data.totalHits > countPerPage) {
+        loadButton.classList.remove('visually-hidden');
+        updateSearchParams(data);
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+function onLoadButtonClick(event) {
+  fetchPhotos(inputDefaultValue)
+    .then(data => {
+      console.log(data);
+
+      createMarkup(data);
+      lightbox.refresh();
+      updateSearchParams(data);
+
+      if (totalHits === data.totalHits) {
+        loadButton.classList.add('visually-hidden');
+        return Notify.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
     })
     .catch(error => console.log(error));
 }
@@ -41,20 +83,20 @@ function createMarkup(params) {
         comments,
         downloads,
       }) => {
-        // const countryLanguages = Object.values(languages).join(', ');
-        return `<div class="photo-card"><div class="thumb">
-  <img src="${largeImageURL}" alt="${tags}" loading="lazy" /> </div>
-  <div class="info">
-    <p class="info-item">
+        return `<div class="gallery__photo-card"><div class="gallery__thumb"><a class = "gallery__link" href = ${largeImageURL}>
+        <img src=${webformatURL} alt="${tags}" loading="lazy" /></a>
+   </div>
+  <div class="gallery__info">
+    <p class="gallery__info-item">
       <b>Likes:</b>${likes}
     </p>
-    <p class="info-item">
+    <p class="gallery__info-item">
       <b>Views:</b>${views}
     </p>
-    <p class="info-item">
+    <p class="gallery__info-item">
       <b>Comments:</b>${comments}
     </p>
-    <p class="info-item">
+    <p class="gallery__info-item">
       <b>Downloads:</b>${downloads}
     </p>
   </div>
@@ -64,4 +106,16 @@ function createMarkup(params) {
     .join('');
 
   gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function makeInitialValues() {
+  loadButton.classList.add('visually-hidden');
+  gallery.innerHTML = '';
+  countOfPage = 1;
+  totalHits = null;
+}
+
+function updateSearchParams(value) {
+  totalHits += value.hits.length;
+  countOfPage += 1;
 }
